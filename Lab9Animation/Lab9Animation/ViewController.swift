7463fb961ff37e155ed96c64bb573106f6abcb5e
DUC NGUYEN <nduc@10-150-44-123.dhcp-m.deakin.edu.au>
2017-05-11 11:01:17 +1000

    Lab 9 - Version 2

--- Lab9Animation/Lab9Animation/ViewController.swift
+++ Lab9Animation/Lab9Animation/ViewController.swift
@@ -13,7 +13,21 @@ class ViewController: UIViewController {
     var gameTimer: Timer?
     var gravity: UIGravityBehavior?
     var animator: UIDynamicAnimator?
+    var img = [UIImage(named: "redballoon"),
+               UIImage(named: "greenballoon"),
+               UIImage(named: "blueballoon")]
+    var speed: Double?
+    var count: Int = 0
     
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var speedSlider: UISlider!
+    @IBAction func speedControl(_ sender: UISlider) {
+        speed = Double(speedSlider.value)
+        let vector = CGVector(dx: 0.0, dy: speed!)
+        print("\(speed)")
+        gravity?.gravityDirection = vector
+        animator?.addBehavior(gravity!)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +38,7 @@ class ViewController: UIViewController {
         gravity = UIGravityBehavior(items: [])
         
         let vector = CGVector(dx: 0.0, dy: 0.1)
+        print("\(speed)")
         gravity?.gravityDirection = vector
         animator?.addBehavior(gravity!)
     }
@@ -35,9 +50,10 @@ class ViewController: UIViewController {
 
     func addBalloons(_ : Any){
         let xCoordinate = arc4random() % UInt32(self.view.bounds.width)
+        let index = Int(arc4random_uniform(3))
         //Create a button, set Image, assign touchUpInside handler, add it to the view and gravity animator
         let btn = UIButton(frame: CGRect(x: Int(xCoordinate), y: 60, width: 50, height: 50))
-        btn.setImage(UIImage(named: "balloon"), for: .normal)
+        btn.setImage(img[index], for: .normal)
         btn.addTarget(self, action: #selector(self.didPopBalloon(sender:)), for: .touchUpInside)
         self.view.addSubview(btn)
         
@@ -48,6 +64,9 @@ class ViewController: UIViewController {
     func didPopBalloon(sender: UIButton){
         sender.setImage(UIImage(named: "exploded"), for: .normal)
         UIView.animate(withDuration: 0.4, animations: {sender.alpha = 0}, completion: {(true) in sender.removeFromSuperview()})
+        count += 1
+        scoreLabel.text = "Score: \(count) balloons"
+        scoreLabel.sizeToFit()
     }
 
 }
